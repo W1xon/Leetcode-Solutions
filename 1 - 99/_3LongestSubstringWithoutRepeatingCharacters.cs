cc327a34428eb8c47efe7ce59536da4a580706dc
Wixon <vladimir16buzo@gmail.com>
2026-08-03 06:53:17 +0000

    feat: add solution for Longest Substring Without Repeating Characters

---
+++ 1 - 99/_3LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,26 @@
+namespace LeetCode._1___99;
+
+public class _3LongestSubstringWithoutRepeatingCharacters
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        Span<int> symbols = stackalloc int[256];
+        int maxLen = 0;
+        for (int left = 0, right = 0; right < s.Length; right++)
+        {
+            char current = s[right];
+
+            if (symbols[current] > left)
+            {
+                left = symbols[current];
+            }
+
+            symbols[current] = right + 1;
+
+            int len = right - left + 1;
+            maxLen = len > maxLen ? len : maxLen;
+        }
+
+        return maxLen;
+    }
+}
